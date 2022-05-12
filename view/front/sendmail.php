dc3191af38c8e49733042a5dc0829dd2e75360eb
youssefkh2 <youssef.khemakhem@esprit.tn>
2022-05-12 09:19:40 +0100

    yayoo

---
+++ view/front/sendmail.php
@@ -0,0 +1,335 @@
+<?php
+	session_start() ; 
+	if (isset($_SESSION["username"]))
+	{
+		if ($_SESSION["role_user"] == "Administrateur")
+			header("location:../back/crudUtilisateur.php") ; 
+		else if ($_SESSION["role_user"] == "User")
+		header("location:../back/crudUtilisateur.php") ; 
+	}
+    if (isset($_POST['keysent']) && isset($_POST['newpassword']) && isset($_POST['keywritten'])  ) {
+       
+        if ($_POST['keysent'] == $_POST['keywritten']) {
+           $password = $_POST['newpassword'];
+        $id = $_POST['id_user'];
+        include_once '../../Controller/UtilisateurC.php';
+        $userC = new UtilisateurC();
+        $userC->changePass($password,$id);
+        header("location:login.php") ; 
+       }else {
+        header("location:forgetpassword.php?error=2") ;
+       }
+        
+    }
+
+    if (isset($_POST['email'])) {
+        $email = $_POST["email"] ; 
+        $key = $_POST['key'];
+        $con = mysqli_connect('localhost','root') ; 
+        mysqli_select_db($con,'diversify') ;
+
+		$req = "select * from utilisateurs where email_user='$email'" ; 
+		$result = $con->query($req) ; 
+
+		if ($result->num_rows>0) 
+		{
+			//$_SESSION['username'] = $username ;
+			while ($row = $result->fetch_assoc()) 
+			{
+                        $nom_prenom_user = $row['nom_user'] ." " .$row['prenom_user'];
+                        $username = $row['username'] ;
+                        $id = $row['id_user'] ;
+			}
+
+
+			/*if ($_SESSION["role_user"] == "Administrateur")
+				header("location:../back1/View/crudUtilisateur.php") ; 
+			else if ($_SESSION["role_user"] == "User")
+				header("location:user.php") ; 
+			die ; */
+
+
+            //---------------------sending mail---------------//
+            
+require '../../PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isMail();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'aveaveyro15@gmail.com';                 // SMTP username
+$mail->Password = 'ellaba200';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('noreply@techjob.com', 'no-reply');
+$mail->addAddress($email, $username);     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);   
+  
+$mail->Subject = 'mot de passe oublié';
+$mail->Body = 'Hi,<br>
+Someone has requested a password recovery link for your account. If you requested this, you can obtain a new password through the code below.<br>'.$key.'<br>If you didnt request this, please ignore this email. Your password wont change unless someone else know the key.';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    
+
+//---------------------end sending mail -----------//
+
+
+		
+            ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- SITE TITTLE -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Classimax</title>
+  
+  <!-- FAVICON -->
+  <link href="img/favicon.png" rel="shortcut icon">
+  <!-- PLUGINS CSS STYLE -->
+  <!-- <link href="plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet"> -->
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap-slider.css">
+  <!-- Font Awesome -->
+  <link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <!-- Owl Carousel -->
+  <link href="plugins/slick-carousel/slick/slick.css" rel="stylesheet">
+  <link href="plugins/slick-carousel/slick/slick-theme.css" rel="stylesheet">
+  <!-- Fancy Box -->
+  <link href="plugins/fancybox/jquery.fancybox.pack.css" rel="stylesheet">
+  <link href="plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+  <!-- CUSTOM CSS -->
+  <link href="css/style.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+
+<body class="body-wrapper">
+
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <nav class="navbar navbar-expand-lg navbar-light navigation">
+            <a class="navbar-brand" href="index.html">
+              <img src="images/hj.png" alt="" width="193" height="38"> 
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto main-nav ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Acceuil</a>
+                </li>
+                <li class="nav-item  ">
+                  <a class="nav-link" href="about-us.html">Description</a>
+                </li>
+                <li class="nav-item dropdown dropdown-slide">
+                  <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Pages <span><i class="fa fa-angle-down"></i></span>
+                  </a>
+                  <!-- Dropdown list -->
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="contact-us.html">Contacter Nous</a>
+                    <a class="dropdown-item" href="user-profile.html">Profil D'utilisateur</a>
+                  </div>
+                </li>
+                
+              </ul>
+              <ul class="navbar-nav ml-auto mt-10">
+                <li class="nav-item">
+                  <a class="nav-link login-button" href="login.html">Connexion</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white add-button" href="ad-listing.html"><i class="fa fa-plus-circle"></i> Ajouter Service</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<section class="login py-5 border-top-1">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-8 align-item-center">
+                <div class="border">
+                    <h3 class="bg-gray p-4">change your password</h3>
+                    <form action="" method="POST">
+                        <fieldset class="p-4">
+                            <input type="text" name="id_user" value="<?php echo $id; ?>" hidden>
+                            <input type="text" name="keysent" value="<?php echo $key; ?>" hidden>
+                            <input type="text" name="keywritten" placeholder="key ..." class="border p-3 w-100 my-2">
+                            <input type="password" name="newpassword" placeholder="new Password" class="border p-3 w-100 my-2">
+                            <div class="loggedin-forgot">
+                                    <input type="checkbox" id="keep-me-logged-in">
+                                    <label for="keep-me-logged-in" class="pt-3 pb-2">Keep me logged in</label>
+                            </div>
+                            <button type="submit" name="change password" class="d-block py-3 px-5 bg-primary text-white border-0 rounded font-weight-bold mt-3">change password</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--============================
+=            Footer            =
+=============================-->
+
+<footer class="footer section section-sm">
+  <!-- Container Start -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-7 offset-md-1 offset-lg-0">
+        <!-- About -->
+        <div class="block about">
+          <!-- footer logo -->
+          <img src="images/logo-footer.png" alt="">
+          <!-- description -->
+          <p class="alt-color">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+      <!-- Link list -->
+      <div class="col-lg-2 offset-lg-1 col-md-3">
+        <div class="block">
+          <h4>Site Pages</h4>
+          <ul>
+            <li><a href="#">Boston</a></li>
+            <li><a href="#">How It works</a></li>
+            <li><a href="#">Deals & Coupons</a></li>
+            <li><a href="#">Articls & Tips</a></li>
+            <li><a href="terms-condition.html">Terms & Conditions</a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- Link list -->
+      <div class="col-lg-2 col-md-3 offset-md-1 offset-lg-0">
+        <div class="block">
+          <h4>Admin Pages</h4>
+          <ul>
+            <li><a href="category.html">Category</a></li>
+            <li><a href="single.html">Single Page</a></li>
+            <li><a href="store.html">Store Single</a></li>
+            <li><a href="single-blog.html">Single Post</a>
+            </li>
+            <li><a href="blog.html">Blog</a></li>
+
+
+
+          </ul>
+        </div>
+      </div>
+      <!-- Promotion -->
+      <div class="col-lg-4 col-md-7">
+        <!-- App promotion -->
+        <div class="block-2 app-promotion">
+          <div class="mobile d-flex">
+            <a href="">
+              <!-- Icon -->
+              <img src="images/footer/phone-icon.png" alt="mobile-icon">
+            </a>
+            <p>Get the Dealsy Mobile App and Save more</p>
+          </div>
+          <div class="download-btn d-flex my-3">
+            <a href="#"><img src="images/apps/google-play-store.png" class="img-fluid" alt=""></a>
+            <a href="#" class=" ml-3"><img src="images/apps/apple-app-store.png" class="img-fluid" alt=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Container End -->
+</footer>
+<!-- Footer Bottom -->
+<footer class="footer-bottom">
+  <!-- Container Start -->
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-12">
+        <!-- Copyright -->
+        <div class="copyright">
+          <p>Copyright © <script>
+              var CurrentYear = new Date().getFullYear()
+              document.write(CurrentYear)
+            </script>. All Rights Reserved, theme by <a class="text-primary" href="https://themefisher.com" target="_blank">themefisher.com</a></p>
+        </div>
+      </div>
+      <div class="col-sm-6 col-12">
+        <!-- Social Icons -->
+        <ul class="social-media-icons text-right">
+          <li><a class="fa fa-facebook" href="https://www.facebook.com/themefisher" target="_blank"></a></li>
+          <li><a class="fa fa-twitter" href="https://www.twitter.com/themefisher" target="_blank"></a></li>
+          <li><a class="fa fa-pinterest-p" href="https://www.pinterest.com/themefisher" target="_blank"></a></li>
+          <li><a class="fa fa-vimeo" href=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- Container End -->
+  <!-- To Top -->
+  <div class="top-to">
+    <a id="top" class="" href="#"><i class="fa fa-angle-up"></i></a>
+  </div>
+</footer>
+
+<!-- JAVASCRIPTS -->
+<script src="plugins/jQuery/jquery.min.js"></script>
+<script src="plugins/bootstrap/js/popper.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap-slider.js"></script>
+  <!-- tether js -->
+<script src="plugins/tether/js/tether.min.js"></script>
+<script src="plugins/raty/jquery.raty-fa.js"></script>
+<script src="plugins/slick-carousel/slick/slick.min.js"></script>
+<script src="plugins/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+<script src="plugins/fancybox/jquery.fancybox.pack.js"></script>
+<script src="plugins/smoothscroll/SmoothScroll.min.js"></script>
+<!-- google map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places"></script>
+<script src="plugins/google-map/gmap.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+
+</html>
+
+<?php }
+    }
+}
+?>
